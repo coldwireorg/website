03216abc3bd62eb90355fb982397b14a99791298
monoko <monoko@riseup.net>
2022-02-04 15:28:35 +0100

    improve responsive

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ func init() {
 
 func main() {
 	engine := html.New("./views", ".html")
+	engine.Reload(true)
+
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
@@ -27,34 +29,59 @@ func main() {
 	app.Static("/static", "./static")
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{}, "layouts/home")
+		return c.Render("index", fiber.Map{
+			"nav": fiber.Map{
+				"color": "green",
+			},
+			"infos": fiber.Map{},
+		}, "layouts/home")
 	})
 
 	app.Get("/join", func(c *fiber.Ctx) error {
 		return c.Render("pages/join", fiber.Map{
-			"title":       "Join",
-			"description": "Write articles, communicate, give your voice, design or contribute to our projets!",
+			"nav": fiber.Map{
+				"color": "white",
+			},
+			"infos": fiber.Map{
+				"title":       "Join",
+				"description": "Write articles, communicate, give your voice, design or contribute to our projets!",
+			},
 		}, "layouts/page")
 	})
 
 	app.Get("/blog", func(c *fiber.Ctx) error {
 		return c.Render("pages/blog", fiber.Map{
-			"title":       "",
-			"description": "",
+			"nav": fiber.Map{
+				"color": "white",
+			},
+			"infos": fiber.Map{
+				"title":       "",
+				"description": "",
+			},
 		}, "layouts/page")
 	})
 
 	app.Get("/about", func(c *fiber.Ctx) error {
 		return c.Render("pages/about", fiber.Map{
-			"title":       "About",
-			"description": "Who are we, what do we do and why it is a legitimate question?",
+			"nav": fiber.Map{
+				"color": "white",
+			},
+			"infos": fiber.Map{
+				"title":       "About",
+				"description": "Who are we, what do we do and why it is a legitimate question?",
+			},
 		}, "layouts/page")
 	})
 
 	app.Get("/donate", func(c *fiber.Ctx) error {
 		return c.Render("pages/donate", fiber.Map{
-			"title":       "Donate",
-			"description": "Help us building resilient and safe organizing tools!",
+			"nav": fiber.Map{
+				"color": "white",
+			},
+			"infos": fiber.Map{
+				"title":       "Donate",
+				"description": "Help us building resilient and safe organizing tools!",
+			},
 		}, "layouts/page")
 	})
 
